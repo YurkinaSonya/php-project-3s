1b884e4acef0914b088f21de4494dde32e8b0aef
Sonya Yurkina <yurkina.sonya@yandex.ru>
2023-11-24 21:35:47 +0700

    * selectColumn and selectColumnOne

--- core/db/Handler.php
+++ core/db/Handler.php
@@ -16,6 +16,9 @@ interface Handler
 
     public function select(string $sql) : array;
     public function selectOne(string $sql) : array|null;
+
+    public function selectColumn(string $sql, string $columnName) : array;
+    public function selectColumnOne(string $sql, string $columnName) : mixed;
     public function insert($tableName, $values): bool|int;
     public function update(string $tableName, array $values, ?string $where = null): bool|int;
     public function execute(string $sql) : bool|int;
