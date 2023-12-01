95bca3cba906d680f10c75267b0d8d908e167205
Sonya Yurkina <yurkina.sonya@yandex.ru>
2023-12-01 15:03:23 +0700

    * fixes for list of posts

--- app/controller/PostController.php
+++ app/controller/PostController.php
@@ -53,75 +53,34 @@ class PostController
         $userId = $this->tokenService->getCurrentUserId();
         $pageSize = $request->getQueryParam('size', $this->pageCount);
         $currentPage = $request->getQueryParam('page', 1);
-        $whereTerms = $this->generateWhereTerms($request, $userId);
-        $order = null;
-        if ($request->getQueryParam('sorting')) {
-            $order = ' ORDER BY ';
-            switch ($request->getQueryParam('sorting')) {
-                case 'CreateDesc':
-                    $order .= 'create_time Desc ';
-                    break;
-                case 'CreateAsc':
-                    $order .= 'create_time Asc ';
-                    break;
-                case 'LikeDesc':
-                    $order .= 'likes Desc ';
-                    break;
-                case 'LikeAsc':
-                    $order .= 'likes Asc ';
-                    break;
-            }
-        }
         $posts = array_map(
             fn($post) => $this->hydratePostDto($post)->toArray(),
             $this->postRepository->getList(
                 $pageSize * ($currentPage - 1),
                 $pageSize,
-                $whereTerms,
-                $order
+                $request->getQueryParam('tags'),
+                $request->getQueryParam('author'),
+                $request->getQueryParam('min'),
+                $request->getQueryParam('max'),
+                $request->getQueryParam('onlyMyCommunities') === 'true',
+                $userId,
+                ($userId !== null) ? $this->subscribeRepository->getSubscribesOfUser($userId) : null,
+                ($userId !== null) ? $this->administratorRepository->getAdminRolesOfUser($userId) : null,
+                $request->getQueryParam('sorting')
             )
         );
-        $total = $this->postRepository->getTotalCount($whereTerms);
+        $total = $this->postRepository->getTotalCount(
+            $request->getQueryParam('tags'),
+            $request->getQueryParam('author'),
+            $request->getQueryParam('min'),
+            $request->getQueryParam('max'),
+            $request->getQueryParam('onlyMyCommunities') === 'true',
+            $userId,
+            ($userId !== null) ? $this->subscribeRepository->getSubscribesOfUser($userId) : null,
+            ($userId !== null) ? $this->administratorRepository->getAdminRolesOfUser($userId) : null
+        );
         $pageInfo = new PageInfoDto($pageSize, ceil($total/$pageSize), $currentPage);
-        return $this->view->render([$posts, $pageInfo->toArray()]);
-    }
-
-    private function generateWhereTerms(Request $request, ?string $userId) : array
-    {
-        /**
-        $onlyMyCommunities = $request->getQueryParam('onlyMyCommunities');
-         */
-        $whereTerms = [];
-        if ($request->getQueryParam('author')) {
-            $whereTerms[] = ' post.author_name LIKE "%' . $request->getQueryParam('author') . '%"';
-        }
-        if ($request->getQueryParam('min')) {
-            $whereTerms[] = ' post.reading_time >= ' . $request->getQueryParam('min');
-        }
-        if ($request->getQueryParam('max')) {
-            $whereTerms[] = ' post.reading_time <= ' . $request->getQueryParam('max');
-        }
-        if ($request->getQueryParam('onlyMyCommunities') === 'true' and $userId !== null) {
-            $subscribes = $this->subscribeRepository->getSubscribesOfUser($userId);
-            $admins =$this->administratorRepository->getAdminRolesOfUser($userId);
-            $arrayOfMyCommunities = [];
-            foreach ($subscribes as $subCommunityId) {
-                $arrayOfMyCommunities[] = $subCommunityId;
-            }
-            foreach ($admins as $adminCommunityId) {
-                if (!in_array($adminCommunityId, $arrayOfMyCommunities)) {
-                    $arrayOfMyCommunities[] = $adminCommunityId;
-                }
-            }
-            $myCommunities = '("' . implode('","', $arrayOfMyCommunities) .'")';
-            //var_export($myCommunities); die;
-            $whereTerms[] = ' post.community_id in ' . $myCommunities;
-        }
-        if ($request->getQueryParam('tags')) {
-            $tags = '("' . implode('","', $request->getQueryParam('tags')) .'")';
-            $whereTerms[] = ' post_tags.tag_id in ' . $tags;
-        }
-        return $whereTerms;
+        return $this->view->render(["posts" => $posts, "pagination" => $pageInfo->toArray()]);
     }
 
 
--- app/repository/PostRepository.php
+++ app/repository/PostRepository.php
@@ -8,25 +8,124 @@ use core\repository\AbstractRepository;
 class PostRepository extends AbstractRepository
 {
 
-    public function getList(int $offset, int $limit, array $whereTerms, ?string $order) : array
+    public function getList(
+        int $offset,
+        int $limit,
+        ?array $tags,
+        ?string $author,
+        ?string $min,
+        ?string $max,
+        ?bool $onlyMyCommunities,
+        ?string $userId,
+        ?array $subscribes,
+        ?array $admins,
+        ?string $sorting
+    ) : array
     {
-        $sql = 'SELECT  post.* ' . $this->sqlWithTerms($whereTerms) . ' GROUP BY post.id ' . $order . ' LIMIT ' . $limit . ' OFFSET ' . $offset;
+        $order = null;
+        $sortingCases = [
+            'CreateDesc' => 'create_time Desc ',
+            'CreateAsc' => 'create_time Asc ',
+            'LikeDesc' => 'likes Desc ',
+            'LikeAsc' => 'likes Asc '
+        ];
+        if ($sorting) {
+            $order = ' ORDER BY post.' . $sortingCases[$sorting];
+        }
+        $terms = $this->sqlWithTerms(
+                        $tags, $author, $min,
+                        $max, $onlyMyCommunities,
+                        $userId, $subscribes, $admins
+                        );
+        $sql = 'SELECT  post.* ' . $terms . $order . ' LIMIT ' . $limit . ' OFFSET ' . $offset;
+        //var_export($sql); die;
         return array_map(fn($row) => Post::fromArray($row), $this->db->select($sql));
     }
 
-    public function getTotalCount(array $whereTerms) : int
+    public function getTotalCount(
+        ?array $tags,
+        ?string $author,
+        ?string $min,
+        ?string $max,
+        ?bool $onlyMyCommunities,
+        ?string $userId,
+        ?array $subscribes,
+        ?array $admins
+    ) : int
     {
-        $sql = 'SELECT COUNT(*) ' . $this->sqlWithTerms($whereTerms);
-        return $this->db->selectColumnOne($sql, 'COUNT(*)');
+        $sql = 'SELECT COUNT(*) as cnt' . $this->sqlWithTerms(
+                                                        $tags, $author, $min,
+                                                        $max, $onlyMyCommunities,
+                                                        $userId, $subscribes, $admins
+                                                        );
+        return $this->db->selectColumnOne($sql, 'cnt');
     }
 
-    private function sqlWithTerms(array $whereTerms) : string
+    private function sqlWithTerms(
+        ?array $tags,
+        ?string $author,
+        ?string $min,
+        ?string $max,
+        ?bool $onlyMyCommunities,
+        ?string $userId,
+        ?array $subscribes,
+        ?array $admins
+    ) : string
     {
-        $sql = ' FROM ' . $this->getTableName() . ' JOIN post_tags ON post.id = post_tags.post_id ';
+        $whereTerms = $this->generateWhereTerms(
+            $tags, $author, $min,
+            $max, $onlyMyCommunities,
+            $userId, $subscribes, $admins
+        );;
+        $sql = ' FROM ' . $this->getTableName();
+        if ($tags) {
+            $sql .= ' JOIN post_tags ON post.id = post_tags.post_id ';
+        }
         if ($whereTerms) {
             $sql.= ' WHERE ' . implode(' AND ', $whereTerms);
         }
+        $sql .= ' GROUP BY post.id ';
         return $sql;
+    }
+
+    private function generateWhereTerms(
+        ?array $tags,
+        ?string $author,
+        ?string $min,
+        ?string $max,
+        ?bool $onlyMyCommunities,
+        ?string $userId,
+        ?array $subscribes,
+        ?array $admins
+    ) : array
+    {
+        $whereTerms = [];
+        if ($tags) {
+            $inTags = '("' . implode('","', $tags) .'")';
+            $whereTerms[] = ' post_tags.tag_id in ' . $inTags;
+        }
+        if ($author) {
+            $whereTerms[] = ' post.author_name LIKE "%' . $author . '%"';
+        }
+        if ($min) {
+            $whereTerms[] = ' post.reading_time >= ' . $min;
+        }
+        if ($max) {
+            $whereTerms[] = ' post.reading_time <= ' . $max;
+        }
+        if ($onlyMyCommunities === true and $userId !== null) {
+            $myCommunitiesIds = [];
+            foreach ($subscribes as $subCommunityId) {
+                $myCommunitiesIds[] = $subCommunityId;
+            }
+            foreach ($admins as $adminCommunityId) {
+                if (!in_array($adminCommunityId, $myCommunitiesIds)) {
+                    $myCommunitiesIds[] = $adminCommunityId;
+                }
+            }
+            $whereTerms[] = ' post.community_id in ("' . implode('", "', $myCommunitiesIds) .'")';
+        }
+        return $whereTerms;
     }
 
     protected function getTableName(): string
