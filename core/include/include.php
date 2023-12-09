f8260e4d0955d1c3775d13d93a2a53f3b28df61f
Sonya Yurkina <yurkina.sonya@yandex.ru>
2023-12-09 21:22:06 +0700

    * 500 status code

--- core/include/include.php
+++ core/include/include.php
@@ -4,6 +4,15 @@ const INC_ROOT = __DIR__ . '/../..';
 const INC_CORE = INC_ROOT . '/core';
 require_once __DIR__ . '/load.php';
 
+set_error_handler(
+/**
+ * @throws ErrorException
+ */ function ($errorNo, $errorStr, $errorFile, $errorLine)
+    {
+        throw new ErrorException($errorStr, $errorNo, 0, $errorFile, $errorLine);
+    }
+);
+
 /** @var $svc \core\ServiceContainer */
 $svc = new core\ServiceContainer();
 
